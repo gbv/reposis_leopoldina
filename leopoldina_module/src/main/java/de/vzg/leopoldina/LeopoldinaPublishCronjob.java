b8fb8c1c046cba7de3d845f31bbc9d05d182f17e
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-02-07 13:53:19 +0100

    Use UserCallable for Session Management

--- leopoldina_module/src/main/java/de/vzg/leopoldina/LeopoldinaPublishCronjob.java
+++ leopoldina_module/src/main/java/de/vzg/leopoldina/LeopoldinaPublishCronjob.java
@@ -8,12 +8,14 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.mycore.access.MCRAccessException;
 import org.mycore.common.MCRException;
+import org.mycore.common.MCRSystemUserInformation;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.mcr.cronjob.MCRCronjob;
 import org.mycore.solr.MCRSolrClientFactory;
+import org.mycore.util.concurrent.MCRFixedUserCallable;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -72,28 +74,35 @@ public class LeopoldinaPublishCronjob extends MCRCronjob {
 
     @Override
     public void runJob() {
-        if (MCRConfiguration2.getString("MCR.Solr.ServerURL").isPresent()) {
-            final SolrClient solrClient = MCRSolrClientFactory.getMainSolrClient();
-            final ModifiableSolrParams params = new ModifiableSolrParams();
-            final LocalDate today = LocalDate.now(ZoneOffset.UTC);
-            final String todayString = today.format(DateTimeFormatter.ISO_DATE);
+        try {
+            new MCRFixedUserCallable<Boolean>(() -> {
+                if (MCRConfiguration2.getString("MCR.Solr.ServerURL").isPresent()) {
+                    final SolrClient solrClient = MCRSolrClientFactory.getMainSolrClient();
+                    final ModifiableSolrParams params = new ModifiableSolrParams();
+                    final LocalDate today = LocalDate.now(ZoneOffset.UTC);
+                    final String todayString = today.format(DateTimeFormatter.ISO_DATE);
 
-            params.set("start", 0);
-            params.set("rows", Integer.MAX_VALUE - 1);
-            params.set("fl", "id");
-            params.set("q", PUBLISH_DATA_FIELD + ":[* TO " + todayString + "] OR (*:* AND !" + PUBLISH_DATA_FIELD + ":*)");
-            params.set("fq", "state:publishable");
+                    params.set("start", 0);
+                    params.set("rows", Integer.MAX_VALUE - 1);
+                    params.set("fl", "id");
+                    params.set("q", PUBLISH_DATA_FIELD + ":[* TO " + todayString + "] OR (*:* AND !" + PUBLISH_DATA_FIELD + ":*)");
+                    params.set("fq", "state:publishable");
 
-            try {
-                final QueryResponse response = solrClient.query(params);
-                response.getResults().stream()
-                        .map(result -> (String) result.get("id"))
-                        .map(MCRObjectID::getInstance)
-                        .forEach(this::releaseDocument);
+                    try {
+                        final QueryResponse response = solrClient.query(params);
+                        response.getResults().stream()
+                            .map(result -> (String) result.get("id"))
+                            .map(MCRObjectID::getInstance)
+                            .forEach(this::releaseDocument);
 
-            } catch (SolrServerException | IOException e) {
-                LOGGER.error("Error while searching embargo documents!", e);
-            }
+                    } catch (SolrServerException | IOException e) {
+                        LOGGER.error("Error while searching embargo documents!", e);
+                    }
+                }
+                return true;
+            }, MCRSystemUserInformation.getSuperUserInstance()).call();
+        } catch (Exception e) {
+            LOGGER.error("Errow while calling cron job", e);
         }
     }
 
